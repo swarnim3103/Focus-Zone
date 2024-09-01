6b96a66306b3cea487c4b2742f78bf0d5b2aff3d
swarnim3103 <swarnim3103@gmail.com>
2024-09-01 08:47:35 +0530

    Changes to be committed: 	new file:   project/src/assets/mobile.glb 	modified:   project/src/sections/About.jsx

--- project/src/sections/About.jsx
+++ project/src/sections/About.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useRef} from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, useGLTF, useAnimations } from "@react-three/drei";
 const AnimatedModel = ({ url }) => {
@@ -16,11 +16,23 @@ const AnimatedModel = ({ url }) => {
   scene.rotation.set(0.5, 1, 0);
   return <primitive object={scene} />;
 };
+const RotatingModel = ({ url }) => {
+  const modelRef = useRef();
+  const { scene } = useGLTF("src/assets/mobile.glb");
+  useFrame(() => {
+    if (modelRef.current) {
+      modelRef.current.rotation.y += 0.01;
+    }
+  });
+
+  return <primitive ref={modelRef} object={scene} />;
+};
+
 
 
 const About = () => {
   return (
-    <div className="bg-gradient-to-b from-customBlue4 to-white flex overflow-hidden">
+    <div className="bg-gradient-to-b from-customBlue4 to-white overflow-hidden">
       <div className="flex flex-1">
       <Canvas
     style={{ height: "800px",
@@ -35,7 +47,20 @@ const About = () => {
         <h1 className="border-8 border-white rounded-lg p-4 bg-gradient-to-r from-customBlue4 to-customBlue2 text-xl  font-extralight">A short attention span refers to the difficulty in maintaining focus on a task or activity for an extended period. This can lead to distractions, reduced productivity, and difficulty completing tasks. It often results from overstimulation, stress, or habitual multitasking and can be exacerbated by digital media and constant notifications.</h1>
       </div>
       </div>
-      
+      <div className="flex flex-1">
+      <Canvas
+    style={{ height: "800px",
+      width: "800px"
+    }} 
+    camera={{ position: [0, 0, 5] }} 
+  >
+    <directionalLight position={[0, 0, 5]} intensity={1} />
+    <RotatingModel url="src/assets/mobile.glb" />
+  </Canvas>
+      <div className="flex-1 flex items-center justify-center mr-20 ">
+        <h1 className="border-8 border-white rounded-lg p-4 bg-gradient-to-r from-customBlue4 to-customBlue2 text-xl  font-extralight">A short attention span refers to the difficulty in maintaining focus on a task or activity for an extended period. This can lead to distractions, reduced productivity, and difficulty completing tasks. It often results from overstimulation, stress, or habitual multitasking and can be exacerbated by digital media and constant notifications.</h1>
+      </div>
+      </div>
     </div>
    
   );
